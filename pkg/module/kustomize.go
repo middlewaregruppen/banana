6b5df9f2e9d8786300fe0df0b146f9867dc8abd4
Amir Mofasser <amimof@users.noreply.github.com>
2023-06-15 10:48:26 +0200

    Write module to `src/` dir with `Save()` func

--- pkg/module/kustomize.go
+++ pkg/module/kustomize.go
@@ -1,11 +1,17 @@
 package module
 
 import (
+	"fmt"
 	"io"
+	"path"
+	"strings"
 
+	"github.com/middlewaregruppen/banana/api/types"
+	"github.com/sirupsen/logrus"
 	"sigs.k8s.io/kustomize/api/krusty"
 	"sigs.k8s.io/kustomize/api/resmap"
 	ktypes "sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kyaml/filesys"
 )
 
 var DefaultKustomizerOptions = &krusty.Options{
@@ -15,22 +21,22 @@ var DefaultKustomizerOptions = &krusty.Options{
 	PluginConfig:      ktypes.DisabledPluginConfig(),
 }
 
-type kustomizeModule struct {
+type KustomizeModule struct {
 	resmap  resmap.ResMap
 	name    string
 	version string
-	// opts   types.ModuleOpts
+	fs      filesys.FileSystem
 }
 
-func (m kustomizeModule) Name() string {
+func (m *KustomizeModule) Name() string {
 	return m.name
 }
 
-func (m kustomizeModule) Version() string {
+func (m *KustomizeModule) Version() string {
 	return m.version
 }
 
-func (m kustomizeModule) Build(w io.Writer) error {
+func (m *KustomizeModule) Build(w io.Writer) error {
 
 	// As Yaml output
 	yml, err := m.resmap.AsYaml()
@@ -44,4 +50,56 @@ func (m kustomizeModule) Build(w io.Writer) error {
 		return err
 	}
 	return err
+}
+
+func (m *KustomizeModule) Save(filepath string) error {
+	moduleSrc := fmt.Sprintf("%s/%s", "modules", m.Name())
+	files, err := m.fs.ReadDir(moduleSrc)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		b, err := m.fs.ReadFile(path.Join(moduleSrc, f))
+		if err != nil {
+			return err
+		}
+		moduleDst := fmt.Sprintf("%s/%s", filepath, f)
+		logrus.Debugf("Writing to %s", moduleDst)
+		err = m.fs.WriteFile(moduleDst, b)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewKustomizeModule(fs filesys.FileSystem, mod types.Module) (*KustomizeModule, error) {
+
+	// Prepend "modules/" if module is local
+	moduleName := mod.Name
+	moduleURL := mod.Name
+	if !IsRemote(moduleURL) {
+		moduleURL = fmt.Sprintf("%s/%s", "modules", mod.Name)
+	}
+
+	// Append ref in URL if version is provided
+	if IsRemote(moduleURL) && len(mod.Version) > 0 {
+		moduleName, _ = getModuleNameFromURL(mod.Name)
+		moduleURL = fmt.Sprintf("%s?ref=%s", strings.TrimRight(mod.Name, "/"), mod.Version)
+	}
+
+	logrus.Debugf("module source is %s", moduleURL)
+
+	k := krusty.MakeKustomizer(DefaultKustomizerOptions)
+	res, err := k.Run(fs, moduleURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KustomizeModule{
+		version: mod.Version,
+		name:    moduleName,
+		resmap:  res,
+		fs:      fs,
+	}, nil
 }
