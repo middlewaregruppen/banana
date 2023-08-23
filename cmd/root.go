68a8151220b72d82ce9c264a9769f59c8063479b
Amir Mofasser <amimof@users.noreply.github.com>
2023-08-23 10:38:03 +0200

    Adds `Component` API
    * Adds `save` command to cli
    * Build  command now builds kustomization in memory using the provided banana file

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/middlewaregruppen/banana/cmd/build"
 	"github.com/middlewaregruppen/banana/cmd/create"
+	"github.com/middlewaregruppen/banana/cmd/save"
 	"github.com/middlewaregruppen/banana/cmd/version"
 	"github.com/sirupsen/logrus"
 
@@ -50,6 +51,7 @@ func NewDefaultCommand() *cobra.Command {
 	c.AddCommand(version.NewCmdVersion(stdOut))
 	c.AddCommand(create.NewCmdCreate(fs))
 	c.AddCommand(build.NewCmdBuild(fs, stdOut))
+	c.AddCommand(save.NewCmdSave(fs, stdOut))
 
 	return c
 }
