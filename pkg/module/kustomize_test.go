a752d130af4add30d3283991b19291880ba28f2f
Amir Mofasser <amimof@users.noreply.github.com>
2023-09-20 19:32:30 +0200

    Adds tests for package `module` (#17)
    * Adds tests for package `module`
    
    * Adds testfile
    
    * Make sure that ingresses pass tests

--- pkg/module/kustomize_test.go
+++ pkg/module/kustomize_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/middlewaregruppen/banana/api/types"
+	"github.com/stretchr/testify/assert"
 	"sigs.k8s.io/kustomize/kyaml/filesys"
 )
 
@@ -76,7 +77,24 @@ func TestKustomizeModuleBuild_Ingresses(t *testing.T) {
 					Prefix: "infra",
 				},
 			},
-			"asd",
+			`apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: test-ingress
+  namespace: test-namespace
+spec:
+  ingressClassName: nginx
+  rules:
+  - host: infra-test-ingress
+    http:
+      paths:
+      - backend:
+          service:
+            name: test-service
+            port:
+              number: 80
+        path: /
+        pathType: Prefix`,
 		},
 	}
 
@@ -88,9 +106,10 @@ func TestKustomizeModuleBuild_Ingresses(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			if buf.String() != tt.want {
-				t.Fatalf("got %s but wanted %s", buf.String(), tt.want)
-			}
+			assert.YAMLEq(t, tt.want, buf.String())
+			// if !assert.YAMLEq(t, tt.want, buf.String()) {
+			// 	t.Fatalf("got\n%s\nbut wanted\n%s", buf.String(), tt.want)
+			// }
 		})
 	}
 
--- pkg/module/module_test.go
+++ pkg/module/module_test.go
@@ -1,14 +1,11 @@
 package module
 
-var kustomizationData = `
-apiVersion: kustomize.config.k8s.io/v1beta1
+var kustomizationData = `apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-- resource.yaml
-`
+- resource.yaml`
 
-var ingressData = `
-apiVersion: networking.k8s.io/v1
+var ingressData = `apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
   name: test-ingress
@@ -16,14 +13,13 @@ metadata:
 spec:
   ingressClassName: nginx
   rules:
-    - host: test-host.domain.com
-      http:
-        paths:
-          - path: /
-            pathType: Prefix
-            backend:
-              service:
-                name: test-service
-                port:
-                  number: 80
-`
+  - host: infra-test-ingress
+    http:
+      paths:
+      - backend:
+          service:
+            name: test-service
+            port:
+              number: 80
+        path: /
+        pathType: Prefix`
