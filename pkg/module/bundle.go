455a9bbf37d7aab246e60b3765308318efc97fcc
Amir Mofasser <amimof@users.noreply.github.com>
2023-10-27 10:55:54 +0200

    Create kustomization.yaml during export (#22)
    * Create kustomization.yaml during export
    
    * Adds `resource.FileName()` to return filename

--- pkg/module/bundle.go
+++ pkg/module/bundle.go
@@ -10,12 +10,14 @@ import (
 
 	"github.com/getsops/sops/v3/aes"
 	"github.com/getsops/sops/v3/keys"
+	"gopkg.in/yaml.v3"
 
 	"github.com/getsops/sops/v3"
 	"github.com/getsops/sops/v3/age"
 	syaml "github.com/getsops/sops/v3/stores/yaml"
 	"sigs.k8s.io/kustomize/api/resmap"
 	"sigs.k8s.io/kustomize/api/resource"
+	ktypes "sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/kustomize/kyaml/filesys"
 	"sigs.k8s.io/kustomize/kyaml/resid"
 	kyaml "sigs.k8s.io/kustomize/kyaml/yaml"
@@ -28,15 +30,25 @@ type Bundle struct {
 	opts          []BundleOpts
 	exportRootDir string
 	recipients    []string
+	kustomization *ktypes.Kustomization
 }
 
 type BundleOpts func(*Bundle) error
 type ExportOpts func(*Bundle) error
 
+// Resources returns a list of Resources on this module
 func (b *Bundle) Resources() []Resource {
 	return b.resources
 }
 
+// AddResource adds a resource to this bundle
+func (b *Bundle) AddResource(res Resource) {
+	b.resources = append(b.resources, res)
+	b.kustomization.Resources = append(b.kustomization.Resources, res.FileName())
+}
+
+// FindByGVK attempts to find a resource with the specific kind group, version and kind
+// specified in the provided GroupVersionKind paramter
 func (b *Bundle) FindByGVK(gvk GroupVersionKind) []*resource.Resource {
 	res := b.resmap.GetMatchingResourcesByAnyId(func(id resid.ResId) bool {
 		if id.Group == gvk.Group && id.Kind == gvk.Kind && id.Version == gvk.Version {
@@ -47,11 +59,17 @@ func (b *Bundle) FindByGVK(gvk GroupVersionKind) []*resource.Resource {
 	return res
 }
 
+// Export writes each resource in the bundle to individual yaml files on the provided filesystem.
+// It effectivly runs resource.Flatten on each resource in this bundle. If a v1.Secret is encountered and
+// a list of age recipient are known, then the resource is encrypted before writing to the file.
 func (b *Bundle) Export(fs filesys.FileSystem, opts ...ExportOpts) error {
 
 	// Apply opts
 	for _, opt := range opts {
-		opt(b)
+		err := opt(b)
+		if err != nil {
+			return err
+		}
 	}
 
 	root := b.mod.Name()
@@ -64,9 +82,27 @@ func (b *Bundle) Export(fs filesys.FileSystem, opts ...ExportOpts) error {
 		return err
 	}
 
+	// Create kustomization.yaml
+	kname := path.Join(root, "kustomization.yaml")
+	kfile, err := fs.Create(kname)
+	if err != nil {
+		return err
+	}
+	defer kfile.Close()
+
+	kdata, err := yaml.Marshal(b.kustomization)
+	if err != nil {
+		return err
+	}
+
+	_, err = kfile.Write(kdata)
+	if err != nil {
+		return err
+	}
+
 	// Write each resource to file on fs
 	for _, res := range b.Resources() {
-		fname := strings.ToLower(path.Join(root, fmt.Sprintf("%s_%s.yaml", res.GetKind(), res.GetName())))
+		fname := strings.ToLower(path.Join(root, res.FileName()))
 		dfile, err := fs.Create(fname)
 		if err != nil {
 			return err
@@ -210,6 +246,7 @@ func WithResMap(rm resmap.ResMap) BundleOpts {
 	}
 }
 
+// WithAgeRecipients configures the bundle adding age recipients used for encryption
 func WithAgeRecipients(recipients []string) BundleOpts {
 	return func(b *Bundle) error {
 		b.recipients = recipients
@@ -217,7 +254,7 @@ func WithAgeRecipients(recipients []string) BundleOpts {
 	}
 }
 
-// ApplyURLs applies all the URLs defined in this module to the provided ResMap.
+// WithURLs applies all the URLs defined in this module to the provided ResMap.
 func WithURLs(s string) BundleOpts {
 	return func(b *Bundle) error {
 		// Create a list of ingress resources to transform
@@ -242,7 +279,7 @@ func WithURLs(s string) BundleOpts {
 	}
 }
 
-// ApplySecrets applies all secrets defined in this module to the provided ResMap.
+// WithSecrets applies all secrets defined in this module to the provided ResMap.
 // Searches through the given resmap for Secret resources, updating/adding secrets on this module.
 // The Secret resource to update is determined by the secret key name itself.
 // This function only adds or updates values in the Secret resource if the key matches that of the module.
@@ -274,9 +311,11 @@ func WithSecrets(secrets []Secret) BundleOpts {
 
 // NewBundle returns a new Bundle for the given module and options provided
 func NewBundle(m Module, opts ...BundleOpts) (*Bundle, error) {
+
 	b := &Bundle{
-		mod:  m,
-		opts: opts,
+		mod:       m,
+		opts:      opts,
+		resources: []Resource{},
 	}
 	// Apply opts
 	for _, opt := range opts {
@@ -284,6 +323,22 @@ func NewBundle(m Module, opts ...BundleOpts) (*Bundle, error) {
 		if err != nil {
 			return nil, err
 		}
+	}
+
+	// Make kustomization for the module
+	kust := &ktypes.Kustomization{
+		TypeMeta: ktypes.TypeMeta{
+			Kind:       ktypes.KustomizationKind,
+			APIVersion: ktypes.KustomizationVersion,
+		},
+		Namespace:  b.mod.Namespace(),
+		Resources:  []string{},
+		Components: []string{},
+	}
+	b.kustomization = kust
+
+	for _, res := range b.resmap.Resources() {
+		b.AddResource(Resource{Resource: res})
 	}
 
 	return b, nil
